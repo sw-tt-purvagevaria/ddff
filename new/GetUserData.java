b2d2373d44f85bdf43099cf9a1d49cea9213054d
purvagevaria <purvagevariya@gmail.com>
2018-03-12 15:50:57 +0530

    Update GetUserData.java

--- new/GetUserData.java
+++ new/GetUserData.java
@@ -1,24 +1,23 @@
 package com.pg.alldemo;
 
-import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Insert;
-import android.arch.persistence.room.Query;
-
-import java.util.List;
-
 /**
- * Created by test on 27/2/18.
+ * Created by test on 12/3/18.
  */
 @Dao
-public interface GetUserData {
-
-    @Insert
-    void insertAllData(List<User> allData);
-
-    @Query("DELETE FROM User")
-    void clearAllTableData();
-
+public interface OperationManager {
 
     @Query("SELECT * FROM user")
     List<User> getAllUserData();
+
+    @Insert
+    void insertData(List<User> user);
+
+    @Insert
+    void insertSingleUserData(User user);
+
+    @Query("SELECT * FROM UserDetail WHERE user_id = :id")
+    UserDetail getUserDetail(int id);
+
+    @Insert
+    void insertSingleUserDetailData(UserDetail userDetail);
 }
